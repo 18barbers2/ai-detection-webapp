622ee516a67b66d309306ad3f540d15150d230d0
18barbers2 <sambarber101@gmail.com>
2024-04-18 19:59:51 -0400

    cleanup app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css'
+import Navbar from './Navbar';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Navbar></Navbar>
     </>
   )
 }
