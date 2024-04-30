9e4fdec8f742f8a65ddfb2669df79692ecc67bcb
18barbers2 <sambarber101@gmail.com>
2024-04-29 20:12:37 -0400

    added disclaimer text with rows and columns

--- src/DisclaimerText.jsx
+++ src/DisclaimerText.jsx
@@ -1,8 +1,19 @@
 function DisclaimerText() {
     return ( 
-        <div className="disclaimer">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At ab tempore aspernatur mollitia eaque enim blanditiis vel dignissimos totam quidem accusamus labore laborum ipsum a ad recusandae distinctio, dolorum quod!
-            Quisquam nihil sapiente accusamus aliquam iste ut aliquid maxime molestias consectetur saepe autem, sint repellendus obcaecati recusandae quis, labore vitae at reiciendis voluptatibus dolore modi illum placeat culpa officia? Et!</p>
+        <div className="disclaimer container">
+            <div className="row">
+                <div className="col">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At ab tempore aspernatur mollitia eaque enim blanditiis vel dignissimos totam quidem accusamus labore laborum ipsum a ad recusandae distinctio, dolorum quod!
+                    Quisquam nihil sapiente accusamus aliquam iste ut aliquid maxime molestias consectetur saepe autem, sint repellendus obcaecati recusandae quis, labore vitae at reiciendis voluptatibus dolore modi illum placeat culpa officia? Et!
+                    </p>
+                </div>
+                <div className="col">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At ab tempore aspernatur mollitia eaque enim blanditiis vel dignissimos totam quidem accusamus labore laborum ipsum a ad recusandae distinctio, dolorum quod!
+                    Quisquam nihil sapiente accusamus aliquam iste ut aliquid maxime molestias consectetur saepe autem, sint repellendus obcaecati recusandae quis, labore vitae at reiciendis voluptatibus dolore modi illum placeat culpa officia? Et!
+                    </p>
+                </div>
+            </div>
+            
         </div>
     );
 }
