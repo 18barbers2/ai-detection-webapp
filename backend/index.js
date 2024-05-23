5d78f891476ff10c797eb92fb0e165e350d817bf
18barbers2 <sambarber101@gmail.com>
2024-05-23 01:25:47 -0400

    edit hello endpoint

--- backend/index.js
+++ backend/index.js
@@ -5,8 +5,8 @@ const port = 5000;
 
 app.use(express.json());
 
-app.post('/hello-world', (req, res) => {
-    res.send('hello, world. Or should I say ... Sam');
+app.get('/hello', (req, res) => {
+    res.send(`hello, world. Or should I say ... Sam`);
 });
 
 app.listen(port, () => {
