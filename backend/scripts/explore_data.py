5226a6551b576f576267fef41437dd84bbc785f1
18barbers2 <sambarber101@gmail.com>
2024-05-24 02:17:39 -0400

    initial exploratory data file being created

---
+++ backend/scripts/explore_data.py
@@ -0,0 +1,37 @@
+import pandas as pd
+
+# @misc {aaditya_bhat_2023,
+# 	author       = { {Aaditya Bhat} },
+# 	title        = { GPT-wiki-intro (Revision 0e458f5) },
+# 	year         = 2023,
+# 	url          = { https://huggingface.co/datasets/aadityaubhat/GPT-wiki-intro },
+# 	doi          = { 10.57967/hf/0326 },
+# 	publisher    = { Hugging Face }
+# }
+
+
+
+def format_data():
+    # original data
+    data = pd.read_csv('../data/GPT-wiki-intro.csv')
+
+    # generated text column
+    ai_data = pd.DataFrame(data['generated_text'])
+    ai_data.rename(columns = { 'generated_text' : 'text' })
+    ai_data['human'] = 0
+
+    # human text column
+    human_data = pd.DataFrame(data['wiki_intro'])
+    human_data.rename(columns = { 'wiki_intro' : 'text' })
+    human_data['human'] = 1
+
+    # combine these rows
+    result = pd.concat([human_data, ai_data], axis=1)
+
+    print(result.head())
+    
+
+
+
+if (__name__ == '__main__'):
+    format_data()
