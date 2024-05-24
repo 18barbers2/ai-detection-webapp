9784dfdf9a8e9c49c87e32457b8ddb5e0aaaefd1
18barbers2 <sambarber101@gmail.com>
2024-05-24 02:36:28 -0400

    prepare data python file setup

---
+++ backend/scripts/prepare_data.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+data = pd.read_csv('../data/processed_data.csv')
+
+
+def stopword_removal():
+    return
+
+
+def lemmatization():
+    return
+
+
+def strip_characters():
+    return 
+
+def stemming():
+    return
+
+
+if (__name__ == '__main__'):
+    stopword_removal()
+    strip_characters()
+    lemmatization()
+    stemming()
