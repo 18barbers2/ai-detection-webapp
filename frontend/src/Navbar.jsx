b5d7348213c5b33e35ee39113bced337be33d0eb
18barbers2 <sambarber101@gmail.com>
2024-08-16 00:15:32 -0400

    include material ui icons

--- frontend/src/Navbar.jsx
+++ frontend/src/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 function Navbar() {
     return (
         <div className='NavBarContainer'>
@@ -7,14 +8,30 @@ function Navbar() {
                 <span className="logo">Frog Social</span>
             </div>
             <div className="NavBarMiddle">
+                <span class="material-symbols-outlined">
+                    search
+                </span>
                 <input placeholder="Search for frogs" />
+                
                 <div className="NavBarLink">Home</div>
+                <span class="material-symbols-outlined">
+                    home
+                </span>
                 <div className="NavBarLink">Create</div>
+                <span class="material-symbols-outlined">
+                    join
+                </span>
                 <div className="NavBarLink">Trade</div>
+                <span class="material-symbols-outlined">
+                    explore
+                </span>
                 <div className="NavBarLink">Community</div>
+                <span class="material-symbols-outlined">
+                    communities
+                </span>
             </div>
             <div className="NavBarRight">
-                <div className="Profile"></div>
+                <img className="Profile"></img>
             </div>
         </div>
     );
