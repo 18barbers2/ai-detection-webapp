3ab465f30a4c1728514f767d0708df0bd0963934
18barbers2 <sambarber101@gmail.com>
2024-08-16 00:01:56 -0400

    basic navbar setup no formatting

--- frontend/src/Navbar.jsx
+++ frontend/src/Navbar.jsx
@@ -2,11 +2,20 @@ import React from 'react';
 
 function Navbar() {
     return (
-        <div className='TopBarContainer'>
-            <div className="TopBarLeft"></div>
+        <div className='NavBarContainer'>
+            <div className="NavBarLeft">
                 <span className="logo">Frog Social</span>
-            <div className="TopBarMiddle"></div>
-            <div className="TopBarRight"></div>
+            </div>
+            <div className="NavBarMiddle">
+                <input placeholder="Search for frogs" />
+                <div className="NavBarLink">Home</div>
+                <div className="NavBarLink">Create</div>
+                <div className="NavBarLink">Trade</div>
+                <div className="NavBarLink">Community</div>
+            </div>
+            <div className="NavBarRight">
+                <div className="Profile"></div>
+            </div>
         </div>
     );
 }
